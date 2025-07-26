8c45c662877bfd4e70786c4663744cd26a8eff17
gabriel santos <gabrieltemp.mobile@gmail.com>
2025-07-26 08:59:13 -0300

    fix: db ctx factory

--- Ambev.DeveloperEvaluation.Infra/Adapters/DesignTimeDbContextFactory.cs
+++ Ambev.DeveloperEvaluation.Infra/Adapters/DesignTimeDbContextFactory.cs
@@ -6,7 +6,7 @@ public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<DatabaseCo
 {
     public DatabaseContext CreateDbContext(string[] args)
     {
-        string connectionString = "Server=179.184.36.51;Database=Siproc_Dev;User Id=Siproc;Password=oL9273$Ys90dF60@;MultipleActiveResultSets=True;TrustServerCertificate=True;";
+        string connectionString = "Server=sqlserver;Database=AmbevDb;User Id=sa;Password=YourStrong@Passw0rd;MultipleActiveResultSets=True;TrustServerCertificate=True;";
 
         var optionsBuilder = new DbContextOptionsBuilder<DatabaseContext>();
         optionsBuilder.UseSqlServer(connectionString, builder =>
